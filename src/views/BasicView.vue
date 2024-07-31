d167e61800251883eea251526e42082c05d0b95d
borgssam <borgssam@gmail.com>
2024-07-31 15:30:39 +0900

    5장 콤포넌트(기본2)

--- src/views/BasicView.vue
+++ src/views/BasicView.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>value값 연결---------------------</h2>
+  <h3>--------------------->value값 연결---------------------</h3>
   <div class="basic">
     <h3>Hello, {{ title }} </h3>   
     <h3>{{ title }}는 수원장안구에 있습니다. </h3>   
@@ -63,6 +63,50 @@
     <button v-bind:style="btn1" >눌러주세요</button>
     <button v-bind:style="btn2" >눌러주세요</button>
   </div>
+  <h3>---------------------제어문 (v-for, v-if, v-else) ---------------------</h3>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th >제품명</th>
+          <th >가격</th>
+          <th >카테고리</th>
+          <th >배송료</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key = "index" v-for="(item, index) in products">
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.category}}</td>
+          <td>{{item.delivery}}</td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+  <div>
+    <!-- v-if 요소를 만드느냐 마느냐 -->
+    <p v-if="true">if참인때</p>
+    <p v-if="false">if거짓인때</p>
+    <!-- v-show는 요소는 만들고, 보여주는지 여부를 처리 -->
+    <p v-show="true">show참인때</p>
+    <p v-show="false">show거짓인때</p>
+  </div>
+
+  <h3>---------------------이벤트(v-on / @)---------------------</h3>
+  <div>
+    <button v-on:click="increaseCounter">클릭(증가)</button>
+    <button @:click="decreaseCounter">클릭(감소)</button>
+    <p>counter:{{counter}}</p>
+    <input type="number" v-model="countValue">
+    <button @:click="applyCounter">적용</button>
+
+    <button @:click="increaseCounter(),showMsg()">증가후 알림창</button>
+    <button @:click="decreaseCounter(),showMsg()">감소후 알림창</button>
+
+
+  </div>
 
   <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
   끝
@@ -100,7 +144,16 @@ export default {
         backgroundColor:'red',
         color:'skyblue',
         fontSize:'32px',
-      }
+      },
+      products: [
+        {"name":"마우스1", "price":2501, "category": "PC용품1", "delivery":1000},
+        {"name":"마우스2", "price":2502, "category": "PC용품2", "delivery":2000},
+        {"name":"마우스3", "price":2503, "category": "PC용품3", "delivery":3000},
+      ],
+      counter: 0,
+      countValue:10,
+
+
     };
   },
   setup() {
@@ -116,11 +169,36 @@ export default {
     
   },
   methods: {
-    
+    increaseCounter(){
+      this.counter = this.counter +1;
+    },
+    decreaseCounter(){
+      this.counter = this.counter -1;
+    },
+    applyCounter(){
+      this.counter = this.countValue;
+    },
+    showMsg(){
+      alert('현재값 = >'+ this.counter );
+    }
   }
 };
 </script>
 
 <style scoped>
+table{
+  border-collapse: collapse;
+  width:100%;
+}
+td, th{
+  border:1px solid #ddd;
+  text-align: left;
+  padding : 8px;
+}
+th{
+  
+  text-align: center;
+  font-weight: 600;
+}
 
 </style>
