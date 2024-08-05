468ee2fc84506ee45c66e2198a51a1683b0deace
borgssam <borgssam@gmail.com>
2024-08-05 14:11:45 +0900

    11. vuex3 사용자입력

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -7,12 +7,18 @@
     </div>
     <button @click="saveUserData1"> 사용자정보저장1</button>
     <button @click="saveUserData2"> 사용자정보저장2</button>
+    <br>
+    <input type="text" v-model="editName">
+    <input type="text" v-model="editEmail">
+    <button @click="saveUser"> 저장</button>
+
+
   </div>
 </template>
 
 <script>
 import {useStore} from 'vuex';
-import {computed} from 'vue';
+import {computed, ref} from 'vue';
 
 export default {
   name: 'UserView',
@@ -21,7 +27,17 @@ export default {
     //vuex에서 user데이터를 가져오기
     const user = computed(()=> store.state.user);
 
+    const editName = ref('');
+    const editEmail = ref('');
+
     //데이터1를 vuex에 저장하는 메서드
+    const saveUser = ()=>{
+      const data = {name: editName.value, email:editEmail.value};
+      console.log(data);
+      
+      store.commit('setUser', data);
+
+    }
     const saveUserData1 = ()=>{
       const data = {name: '이순신', email:'ceo@sea.com'}
       store.commit('setUser', data);
@@ -34,7 +50,7 @@ export default {
     };
 
     return {
-      user, saveUserData1, saveUserData2
+      user, saveUserData1, saveUserData2, saveUser,editName,editEmail,
     };
   },
 };
